72503275147b03e51bea838abb3848b79a71324c
aytacasd <112809494+aytacasd@users.noreply.github.com>
2023-12-02 15:04:43 +0200

    Update config.js
    bsds

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  owner: "963347002052214824",
-  token: "MTEyNjE3NjY4NzAwNTEyMjY0MA.G2je7X.utyKObY-SD259KSIWCb-XavaIDKU_UZp9D66XQ",
-}
+  owner: "963347052214824",
+  token: "MTEyNjE3NjY4NzMjY0MA.G2je7X.utyKObY-SD259KSIWCb-XavaIDKU_UZp9D66XQ",
