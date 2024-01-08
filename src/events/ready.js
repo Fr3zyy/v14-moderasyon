da2bc9e5caa4ff2352d8bc04b2480d4c4d71fbe9
Logetan <140705831+Logetan@users.noreply.github.com>
2024-01-08 15:32:02 +0000

    TWEEN

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-    let activities = [ `Fr3zy moderasyon altyapi.`, `${client.user.username}` ], i = 0;
+    let activities = [ `Tween Aktif!`, `${client.user.username}` ], i = 0;
     setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
 }};
