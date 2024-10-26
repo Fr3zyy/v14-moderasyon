d344da8fc2b918e97d9352d87d57863af44464a3
Fr3zyy <123799898+Fr3zyy@users.noreply.github.com>
2024-10-26 13:44:35 +0300

    Update slowmode.js

--- src/commands/slowmode.js
+++ src/commands/slowmode.js
@@ -1,5 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
